aaea682a8a6831a269647bb2c65cdf523144553e
6yu7h <114726087+6yu7h@users.noreply.github.com>
2023-08-20 14:10:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload()
 {
 	song = loadSound("Piano.mp3");
@@ -14,9 +20,32 @@ function setup()
 	
 	video = createCapture(VIDEO);
 	video.hide();
+	
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log("PoseNet is Intialised");
 }
 
 function draw()
 {
 	image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+		{
+			console.log(results);
+			leftWristX = results[0].pose.leftWrist.x;
+			leftWristY = results[0].pose.leftWrist.y;
+			console.log("left wristX = "+leftWristX+" left wristY = "+leftWristY);
+			
+			rightWristX = results[0].pose.rightWrist.x;
+			rightWristY = results[0].pose.rightWrist.y;
+			console.log("right wristX = "+rightWristX+" right wristY = "+rightWristY);
+		}
 }
